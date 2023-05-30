a93d3d4e9d302c49ec1c7735fa087aaca6312fe2
Konstantin Orobey <kostya.orobei@yandex.ru>
2023-05-30 14:26:29 +0300

    Init commit

---
+++ TreeProject/Models/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProject.Models
+{
+    public class Node
+    {
+        public Guid Key { get; set; }
+        public string Name { get; set; }
+        public List<Node> Nodes { get; set; }
+    }
+}
